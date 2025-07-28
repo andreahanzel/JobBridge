4e697caeac95b9ff9bb36e7866eb514125ec6eac
Joseph <54655055+Uncore007@users.noreply.github.com>
2025-07-28 16:59:55 -0600

    Add User to Models

---
+++ JobBridge/Models/User.cs
@@ -0,0 +1,22 @@
+namespace JobBridge.Data;
+
+public class User
+{
+    public int Id { get; set; }
+    
+    public required string Role { get; set; }
+    
+    public required string FirstName { get; set; }
+
+    public required string LastName { get; set; }
+    
+    public required string Email { get; set; }
+
+    public required string Phone { get; set; }
+
+    public required string Password { get; set; }
+
+    public required DateTime CreatedAt { get; set; }
+
+    public required DateTime UpdatedAt { get; set; }
+}
